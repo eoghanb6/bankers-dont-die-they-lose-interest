8d434ed1fb53d0fcb0b4966920a73b0e98866e9b
Liam Ferris <L.Ferris@kainos.com>
2015-10-01 11:53:12 +0100

    userMenu for addAccount

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,111 @@
 import java.util.Scanner;
+import Structures.*;
 public class Main {
 
+
     public static void main(String[] args) {
+        menuChoice();
+    }
+    public static void menuChoice(){
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Welcome to Bank");
+        System.out.println("Press 1 to create account, press 2 to exit");
+        int menuChoice1 = 0;
+        try{
+        menuChoice1 = myScanner.nextInt(); }
+        catch (Exception Error){
+            System.out.println("Please enter a suitable value");
+            menuChoice();
+        }
+
+        switch (menuChoice1) {
+            case 1:
+                createAccount();
+                break;
+
+            case 2:
+                System.out.println("Bye");
+                System.exit(2);
+                break;
+
+            default:
+                System.out.println("Please enter a suitable value");
+                menuChoice();
+                break;
+        }
+
+    }
+    public static void createAccount(){
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Enter First Name");
+        String firstName = myScanner.nextLine();
+        System.out.println("Enter Last Name");
+        String lastName = myScanner.nextLine();
+        AccountType accountType;
+        accountType = makeAccountType();
+        int savings = addSavings();
+        if(savings == 0 ){
+          Account account = new Account(firstName, lastName, accountType);
+          System.out.println(account.getAccountInfo());
+        }
+        else{
+            Account account = new Account(firstName, lastName, accountType, savings);
+            System.out.println(account.getAccountInfo());
+        }
 
 
     }
-        public void menuChoice(){
-
-            System.out.println("Welcome to Bank");
-            System.out.println("Press 1 to create account, press 2 to exit");
-            Scanner myScanner = new Scanner(System.in);
-            myScanner.nextLine();
-            int menuChoice1 = 0;
-            try{
-            menuChoice1 = myScanner.nextInt(); }
-            catch (Exception Error){
-                System.out.println("Please enter a suitable value");
-                menuChoice();
-            }
-
-            switch (menuChoice1) {
-                case 1:
-                    //create account
-                    break;
-
-                case 2:
-                    System.out.println("Bye");
-                    System.exit(2);
-                    break;
-
-                default:
-                    System.out.println("Please enter a suitable value");
-                    menuChoice();}
+    public static AccountType makeAccountType(){
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Choose account type");
+        System.out.println("1: Standard \t Overdraft: 500;");
+        System.out.println("2: Saver \t \t Overdraft: 0");
+        System.out.println("3: Premium \t \t Overdraft: 3000");
+        int choice = 0;
+        try{
+            choice = myScanner.nextInt(); }
+        catch (Exception Error){
+            System.out.println("Please enter a suitable value");
+            makeAccountType();
         }
+        AccountType accountType = null;
+        switch(choice){
+            case 1: accountType = AccountType.Standard;
+                break;
+            case 2: accountType = AccountType.Saver;
+                break;
+            case 3: accountType = AccountType.Premium;
+                break;
+
+        }
+        if (choice>3){
+            System.out.println("Please enter a suitable value");
+        makeAccountType();}
+
+        return accountType;
+    }
+    public static int addSavings(){
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Add initial savings or press enter for no savings");
+        String input = myScanner.nextLine();
+        int savings = 0;
+        try{
+            savings = Integer.parseInt(input);
+        }
+        catch(Exception e){
+            if (input.equals("")){return 0;}
+            else {
+                System.out.println("Please enter a suitable value");
+                addSavings(); }
+        }
+        if (savings < 0){
+            System.out.println("Must be greater than 0");
+            addSavings(); return 0;}
+        else{
+        return savings;}
+
+
+    }
 }
 
 
--- src/Structures/Account.java
+++ src/Structures/Account.java
@@ -4,7 +4,7 @@ public class Account {
     private String firstName;
     private String lastName;
     private AccountType accountType;
-    private double Savings;
+    private double Savings =0;
     private int overdraft;
 
     public Account(String firstName, String lastName, AccountType accountType){
@@ -26,5 +26,8 @@ public class Account {
             case Premium:   this.overdraft = 3000;
                 break;
         }
+    }
+    public String getAccountInfo(){
+        return "First name:\t\t" + this.firstName + "\nLast name:\t\t" + this.lastName + "\nAccount Type:\t" + this.accountType + "\nSavings:\t\t" + this.Savings + "\nOverdraft:\t\t" + this.overdraft;
     }
 }
