29a86c804c0da4717f447e1363f3e2b2ffb12941
Eoghan Bradley <eoghan.bradley@kainos.com>
2015-10-06 09:38:38 +0100

    created standard tests

--- src/Structures/StandardTests.java
+++ src/Structures/StandardTests.java
@@ -29,6 +29,94 @@ public class StandardTests {
     public void checkSavings(){
         assertEquals( "Balance: \t£" + 500.0 ,a.getSavings());
     }
+    @Test
+    public void checkDepositIsPositive(){
+        try
+        {
+            assertEquals(540, a.deposit(-10));
+            fail();
+        }catch(Exception e)
+        {
+            final String expectedMessage = "Enter Positive Number";
+            assertEquals(expectedMessage, e.getMessage());
+        }
+    }
+
+    @Test
+    public void checkwithdrawisPositive(){
+        try
+        {
+            assertEquals(510, a.withdraw(-10));
+            fail();
+        } catch(Exception e )
+        {
+            final String expectedMessage = "Enter Positive Number";
+            assertEquals(expectedMessage, e.getMessage()) ;
+        }
+
+
+    }
+    @Test
+    public void WithdrawLessThanmax(){
+        double maxWithdrawal = 10000;
+        try
+        {
+            assertEquals("Balance: \t£"+-1.0, a.withdraw(10001));
+            fail();
+        } catch(Exception e )
+        {
+            final String expectedMessage = "Max withdrawl £" + maxWithdrawal;
+            assertEquals(expectedMessage, e.getMessage()) ;
+        }
+
+
+    }
+    @Test
+    public void allow£1lessforsaverwithdrawals(){
+        double maxWithdrawal = 10000;
+        String savings = a.getSavings();
+        double sum = a.getSavingsDouble() + a.getOverdraft() - a.getTransactionCharge() ;
+        double x = 5000;
+        try
+        {
+            assertEquals(sum, a.withdraw(x));
+            fail();
+        } catch(Exception e )
+        {
+            assertEquals("Cannot withdraw £5000.0 , only £1000.0 available as there is a £0.0 transaction charge for Standard accounts. Balance: £500.0", e.getMessage()) ;
+        }
+    }
+
+    @Test
+    public void getSavingsDouble_SuccessTest(){
+        assertEquals(500.0,a.getSavingsDouble(),0.1);
+    }
+
+    @Test
+    public void deposit_SuccessTest(){
+        try
+        {
+            assertEquals("Balance: \t£510.0",a.deposit(10));
+        }catch(Exception e){}
+    }
+
+    @Test
+    public void withdraw_SuccessTest(){
+        try
+        {
+            assertEquals("Balance: \t£490.0",a.withdraw(10));
+        }catch(Exception e){}
+    }
+
+    @Test
+    public void getAccountNumberTest(){
+        assertEquals(1,a.getaccountnumber());
+    }
+
+    @Test
+    public void getAccountType(){
+        assertEquals(AccountType.Standard,a.getAccountType());
+    }
 
 
 
