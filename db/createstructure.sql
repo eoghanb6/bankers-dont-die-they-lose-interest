8eb58d51a0996af2312eb7eda7bb8d4762f2feb8
Eoghan Bradley <eoghan.bradley@kainos.com>
2015-10-08 10:17:01 +0100

    prevented null values within tables

--- db/createstructure.sql
+++ db/createstructure.sql
@@ -1,11 +1,11 @@
 create database new;
 use new;
 
-create table account_type (type_id int primary key auto_increment, account_type varchar(20), overdraft decimal(15,2), transaction_fee decimal (15,2));
+create table account_type (type_id int primary key auto_increment, account_type varchar(20) NOT NULL, overdraft decimal(15,2) NOT NULL, transaction_fee decimal (15,2) NOT NULL);
 
-create table account (first_name varchar(20), last_name varchar(20), account_number int primary key auto_increment, name_id int ,type_id int, balance decimal(15,2));
+create table account (first_name varchar(20) NOT NULL , last_name varchar(20) NOT NULL, account_number int primary key auto_increment NOT NULL, type_id int NOT NULL, balance decimal(15,2) NOT NULL);
 
 alter table account add constraint fk_account_type_id foreign key(type_id) references account_type (type_id);
 
-INSERT into account_type(account_type, overdraft, transaction_fee) values ("Standard", 500, 0),("Saver", 0, 1),("Premium",3000,0)
+INSERT into account_type(account_type NOT NULL, overdraft NOT NULL, transaction_fee NOT NULL) values ("Standard", 500, 0),("Saver", 0, 1),("Premium",3000,0);
 
